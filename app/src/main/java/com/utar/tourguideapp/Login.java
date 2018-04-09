9aa07cb7714d0e7bb54c8b41948db491a16b482b
LQuan <lquan69>
2018-04-09 22:11:02 +0800

    Login and Register Database Complete, but don't have verification yet

--- app/src/main/java/com/utar/tourguideapp/Login.java
+++ app/src/main/java/com/utar/tourguideapp/Login.java
@@ -2,17 +2,36 @@ package com.utar.tourguideapp;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class Login extends Activity {
+
+    private EditText username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-       //Intent i = new Intent(Login.this,Register.class);
-        //startActivity(i);
+        username = (EditText)findViewById(R.id.login_username);
+        password = (EditText)findViewById(R.id.login_password);
+    }
+
+    public void onLogin(View v) {
+
+        String user_name = username.getText().toString();
+        String user_password = password.getText().toString();
+        String method = "Login";
+
+        AuthenticationServer authenticationServer = new AuthenticationServer(this);
+        authenticationServer.execute(method,user_name,user_password);
+    }
+
+    public void onSignUp(View v) {
+
+        Intent intent = new Intent(Login.this, Register.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/utar/tourguideapp/Register.java
+++ app/src/main/java/com/utar/tourguideapp/Register.java
@@ -1,13 +1,37 @@
 package com.utar.tourguideapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class Register extends Activity {
 
+    private EditText username,password,email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        username = (EditText) findViewById(R.id.inputUsername);
+        password = (EditText)findViewById(R.id.inputPassword);
+        email = (EditText) findViewById(R.id.inputEmail);
     }
+
+    public void onReg(View v) {
+        String user_name = username.getText().toString();
+        String user_password = password.getText().toString();
+        String user_email = email.getText().toString();
+
+        String method = "Register";
+        AuthenticationServer authenticationServer = new AuthenticationServer(this);
+        authenticationServer.execute(method,user_name,user_email,user_password);
+    }
+
+    public void onCancel(View v) {
+        Intent intent = new Intent(Register.this, Login.class);
+        startActivity(intent);
+    }
+
 }
